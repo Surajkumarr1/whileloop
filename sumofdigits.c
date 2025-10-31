5d88d6df5749edb25294f0f879af6f478b15acbe
suraj  kumar <singhsurajsi905@gmail.com>
2025-10-31 16:03:36 +0530

    Create sumofdigits.c

---
+++ sumofdigits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int sum = 0;
+    int ld = 0; // ld = last digit
+    
+    while(n != 0){
+        ld = n % 10;
+        sum = sum + ld;
+        n = n / 10;
+    }
+    
+    printf("%d", sum);
+    return 0;
+}
